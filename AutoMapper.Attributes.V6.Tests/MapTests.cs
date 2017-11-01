cb37a45fad66e67ad9cbfe9be6c7815369bd320f
Tyler Moore <tylermoore124@gmail.com>
2017-11-01 14:46:38 -0400

    Refine branch to only update V6 testing

---
+++ AutoMapper.Attributes.V6.Tests/MapTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using AutoMapper.Attributes.TestAssembly.SubclassTests;
+using NUnit.Framework;
+
+namespace AutoMapper.Attributes.Tests
+{
+    [TestFixture]
+    public abstract class MapTests
+    {
+        protected const string Grandma = "Grandma";
+
+        [SetUp]
+        public void Setup()
+        {
+            Mapper.Initialize(config =>
+            {
+                typeof(Person).GetTypeInfo().Assembly.MapTypes(config);
+            });
+        }
+    }
+}
